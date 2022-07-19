8234ea271c68d1eda576a2e995f3375f7662fa31
faeezs <faeezs@gmail.com>
2022-07-19 17:13:03 +0700

    Add posts index page and slug page

--- src/routes/projects/[slug].svelte
+++ src/routes/projects/[slug].svelte
@@ -21,18 +21,18 @@
 </script>
 
 <svelte:head>
-	<title>My Portfolio | {project.name}</title>
+	<title>My Portfolio | {project?.name}</title>
 </svelte:head>
 
 <div class="sm:-mx-5 md:-mx-10 lg:-mx-20 xl:-mx-38 mb-5">
-	<img class="rounded-lg" src={project.image[0].url} alt={project.title} />
+	<img class="rounded-lg" src={project?.image[0].url} alt={project?.title} />
 </div>
 
-<h1 class="text-4xl font-semibold mb-5">{project.name}</h1>
+<h1 class="text-4xl font-semibold mb-5">{project?.name}</h1>
 
 <div class="mb-5 flex justify-between">
 	<div>
-		{#if project.tags}
+		{#if project?.tags}
 			{#each project.tags as tag}
 				<span class="badge badge-primary mr-2 hover:bg-primary-focus cursor-pointer">{tag}</span>
 			{/each}
@@ -41,10 +41,12 @@
 </div>
 
 <div class="mb-5 prose flex prose-a:text-primary hover:prose-a:text-primary-focus">
-	<a class="mr-5" href={project.demo}>Demo</a>
-	<a href={project.sourceCode}>Source Code</a>
+	<a class="mr-5" href={project?.demo}>Demo</a>
+	<a href={project?.sourceCode}>Source Code</a>
 </div>
 
 <article class="prose prose-xl">
-	{@html marked(project.description)}
+	{#if project?.description}
+		{@html marked(project.description)}
+	{/if}
 </article>
